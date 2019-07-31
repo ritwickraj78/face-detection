54392ee7cc9f6f47e3b80f2c32b46a945b19e750
RitwickRaj78 <ritwickraj78@gmail.com>
2019-07-31 11:43:14 +0530

    cv2 added

---
+++ script.py
@@ -0,0 +1,10 @@
+import cv2
+detect = cv2.CascadeClassifier("1.2 haarcascade_frontalface_default.xml")
+imp_img = cv2.VideoCapture("elon.jpg")
+res, img = imp_img.read()
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces = detect.detectMultiscale(gray,1.3,5)
+cv2.imshow("elon image", img)
+cv2.waitKey(0)
+imp_img.release()
+cv2.destroyAllWindows()
