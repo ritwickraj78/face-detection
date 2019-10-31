0827cf81d59aa21827222c865bc389cba0df5c31
unknown <ritwickraj78@gmail.com>
2019-11-01 00:27:11 +0530

    error for scale coversion resolved

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ detect = cv2.CascadeClassifier("1.2 haarcascade_frontalface_default.xml")
 imp_img = cv2.VideoCapture("elon.jpg")
 res, img = imp_img.read()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = detect.detectMultiscale(gray, 1.3, 5)
+faces = detect.detectMultiScale(gray, 1.3, 5)
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 5)
 cv2.imshow("elon image", img)
